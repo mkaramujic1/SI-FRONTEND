71c825479d8e13bf987d5a3d80a58ccea883bbc9
remima <mkovacevic1@etf.unsa.ba>
2019-04-30 11:50:33 +0200

    Kreiranje prikaza osoblja

--- reactapp/src/PAPA/Components/Dashboard/OptionsComponent.js
+++ reactapp/src/PAPA/Components/Dashboard/OptionsComponent.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './CommonCss.css';
 
 export class Options extends Component {
+	constructor() {
+        super();
+        this.state = {
+          povratak:false
+        };
+        this.handleSubmit=this.handleSubmit.bind(this);
+      }
+    povratak=false;
+    handleSubmit(){// = evt => {
+        //this.setState({povratak:true});
+        console.log("Uslo je u funnnn");
+        var x=document.getElementsByClassName("osoblje");
+    	if(x[0].style.display=="none"){
+    		x[0].style.display="inline";
+    	}
+    	else{
+    		x[0].style.display="none";
+    	}
+      }
     render() {
       return(
         <div style={{ margin: '0px 0', padding: '15px'}}>
@@ -9,7 +28,16 @@ export class Options extends Component {
                 <div class="card-header">Više informacija:</div>
                 <div class="card-body">
                 <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action">Osoblje na predmetima</a>
+                <div class="informacije-osoblja">
+                    		<button onClick={this.handleSubmit} class="list-group-item list-group-item-action">Osoblje na predmetima</button>
+                    		<div class="osoblje" style={{display: 'none'}}>
+                    			<p>Zlatan Tucaković</p>
+                    			<p>Vedran Karihodžić</p>
+                    			<p>Marina Miličević</p>
+                    			<p>Mehmed Brkić</p>
+                    			<p>Emir Cogo</p>
+                 			</div>
+             			</div>
                     <a href="#" class="list-group-item list-group-item-action">Podnošenje zahtjeva</a>
                 </div>
                 </div>
