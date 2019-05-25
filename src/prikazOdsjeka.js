799f48d287da116542df1ba44269fe382de3e052
samrabejtic <sbejtic1@etf.unsa.ba>
2019-05-25 23:41:20 +0200

    Osposobljen prikaz odsjeka

--- src/prikazOdsjeka.js
+++ src/prikazOdsjeka.js
@@ -8,34 +8,54 @@ class prikazOdsjeka extends Component{
 
         this.state = {
           lista: [], 
-          selectedValue: ''
+          search: ''
         }
     }
 
-    componentDidMount(){
+    componentDidMount(search){
 
-        //Promijeniti URL
-        //http://localhost:31901/api/odsjek/GetOdsjeci
-        axios.get ('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            console.log("Lista: ", response.data);
-            this.setState({lista: response.data});
-        })
-        . catch (error =>{
-            console.log(error)
-        })
+        if(search==''){
+            //Promijeniti URL
+            //http://localhost:31901/api/odsjek/GetOdsjeci
+            axios.get ('http://localhost:31901/api/odsjek/GetOdsjeci')
+            .then(response => {
+                console.log("Lista: ", response.data);
+                this.setState({lista: response.data});
+            })
+            . catch (error =>{
+                console.log(error)
+            })
+        }
+        else{
+            axios.get ('http://localhost:31901/api/odsjek/GetOdsjek?id='+search)
+            .then(response => {
+                console.log("Lista: ", response.data);
+                this.setState({lista: response.data});
+            })
+            . catch (error =>{
+                console.log(error)
+                alert("NEMA");
+            })
+        }
     }
 
-    onChange = (e) => {
-        this.setState({selectedValue: e.target.value})
+    handleChange = (e) =>{
+        this.setState({
+          search:e.target.value
+        }) 
     }
 
     render (){
-        const {lista, selectedValue}=this.state
+        const {lista, search}=this.state
+        console.log("l ",lista.length);
         return (
-            <div className="col-md-6">
-            <br />
-                <p>Prikaz svih odsjeka: </p><br />
+            <div className="col-md-7">
+                <br /> 
+                    <input type="text" className="form-control col-md-2" value={search} onChange={this.handleChange}></input>  <br />
+                    <button className="btn btn-success btn-block col-md-2" onClick={()=> this.componentDidMount(search)}>Search</button>
+                <br />
+                
+
                 <table >
                     <thead className="table table-sm table-primary">
                         <tr>
@@ -45,30 +65,19 @@ class prikazOdsjeka extends Component{
                     </thead>
                     <tbody className="table table-sm table-light">
                         {
-                            //paziti sta se prikazuje, nece biti list.title!!!
-                            //ako je length!=0 prikazati listu, u suprotnom vratiti null
-                            //PARAMETRI: list.idOdsjek, list.naziv
                             lista.length ? lista.map(list => 
-                                <tr key={list.id}>
-                                    <th>{list.id}</th>
-                                    <th>{list.title}</th>
+                                <tr key={list.idOdsjek}>
+                                    <th>{list.idOdsjek}</th>
+                                    <th>{list.naziv}</th>
                                 </tr>
                             ): null
                         }
                     </tbody>
                 </table><br /><br />
                 
-                <input type="submit" value="Edit" className="btn btn-success btn-block" />
             </div>
         );
     }
 }
 
-/*<select className="custom-select" value={selectedValue} onChange={this.onChange}> 
-                {
-                    //paziti sta se prikazuje, nece biti list.title!!!
-                    //ako je length!=0 prikazati listu, u suprotnom vratiti null
-                    lista.length ? lista.map(list => <option key={list.id}>{list.title}</option>): null
-                }
-                </select><br /><br /> */
 export default prikazOdsjeka
