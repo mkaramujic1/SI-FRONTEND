28829a0f2b93c43ee3c06a1c557a9b8e1bf1976c
samrabejtic <sbejtic1@etf.unsa.ba>
2019-05-26 23:55:44 +0200

    Prikaz predmeta

---
+++ src/prikazPredmeta.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+class prikazAsistenta extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+          lista: [], 
+          search: ''
+        }
+    }
+
+   
+
+    componentDidMount(param){
+      var xhttp = new XMLHttpRequest();
+      var self = this;
+      
+     xhttp.onreadystatechange = function(){
+        if (xhttp.readyState == 4 && xhttp.status == 200){
+            self.setState({
+            lista: JSON.parse(this.response)
+          });
+        }
+      }
+    
+     if(param!='') xhttp.open("get", "http://localhost:31901/api/predmet/GetPredmet?naziv="+param, true);
+     else xhttp.open("get", "http://localhost:31901/api/predmet/GetPredmeti", true);
+     
+      xhttp.send();
+    }
+
+    handleChange = (e) =>{
+      this.setState({
+        search:e.target.value
+      }) 
+    }
+
+    render (){
+        const {lista, search}=this.state
+        console.log("LISTA", lista.length);
+        return (
+            <div>
+              <br /> 
+                <input type="text" className="form-control col-md-2" value={search} onChange={this.handleChange}></input>  <br />
+                <button className="btn btn-success btn-block col-md-2" onClick={()=> this.componentDidMount(search)}>Search</button>
+              <br />
+              
+              {"id":6,"idAsistent":237,"idProfesor":247,"naziv":"IEK","ects":5,"brojPredavanja":30,"brojVjezbi":30,"opis":""
+              <table>  
+                <thead className="table table-sm table-primary">
+                  <tr>
+                      <th >ID</th>
+                      <th >NAZIV</th>
+                      <th >ECTS</th>
+                      <th >BROJ PREDAVANJA</th>
+                      <th >BROJ VJEŽBI</th>
+                      <th >OPIS</th>
+                      <th >ID PROFESORA</th>
+                      <th >ID ASISTENTA</th>
+                  </tr>
+                </thead>
+                <tbody className="table table-sm table-light">
+                {
+                    lista.length ? lista.map(list => 
+                        <tr key={list.id}>
+                            <th>{list.id}</th>
+                            <th>{list.naziv}</th>
+                            <th>{list.ects}</th>
+                            <th>{list.brojPredavanja}</th>
+                            <th>{list.brojVjezbi}</th>
+                            <th>{list.opis}</th>
+                            <th>{list.idProfesor}</th>
+                            <th>{list.idAsistent}</th>
+                        </tr>)
+                    : null
+                }
+                </tbody>
+              </table><br /><br />
+              
+            </div>
+        );
+    }
+}
+
+export default prikazAsistenta
